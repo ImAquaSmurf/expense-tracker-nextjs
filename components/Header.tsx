dc76cd22e680b2e33a180251f50b5164801d3d64
Axiemeon <axiemeon2001@gmail.com>
2024-09-20 15:08:49 +1000

    First Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,14 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import { checkUser } from '@/lib/checkUser';
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
 
 const Header = async () => {
   const user = await checkUser();
 
   return (
-    <nav className='navbar'>
-      <div className='navbar-container'>
-        <h2>Expense Tracker</h2>
-        <div>
+    <nav className="navbar bg-blue-600 p-4 shadow-md">
+      <div className="navbar-container flex justify-between items-center">
+        <h2 className="text-white text-2xl font-bold">Expense Tracker</h2>
+        <div className="flex items-center space-x-4">
           <SignedOut>
             <SignInButton />
           </SignedOut>
